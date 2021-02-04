9e965c8b15e525fec3c62a473332981b43936753
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2021-02-04 18:44:56 +0000

    Started adding FPS and ticks

--- game.js
+++ game.js
@@ -1,15 +1,30 @@
-var gl; // gl from canvas
-
 window.onload = function() {
-    gl = new glFromCanvas(document.querySelector("#glCanvas"), draw);
-}
 
-function draw(gl) {
-    gl.do.clearScreen(gl, [0.0, 0.0, 0.0, 0.0]);
-    gl.do.clearBuffers(gl);
-    gl.do.addShape(gl, new shapes.Cuboid(-1, -1, -1, 1, 1, 1));
-    gl.do.addShape(gl, new shapes.Cuboid(0, 0, 0, 1, 1, 1));
-    gl.do.loadBuffers(gl);
-    gl.do.loadTexture(gl, "cubetexture.png");
-    gl.do.draw(gl, [0.7, [1, 0, 1], [0, 0, 0]]);
+    var game = { // Main game mechanics, including timing
+        lastTick: 0, // Changes to keep time logged
+        counter: 0,
+        canvas: document.querySelector("#gameCanvas"),
+        cWidth: 809,
+        cHeight: 500,
+        ctx: document.querySelector("#gameCanvas").getContext("2d"),
+        tick: function(thisTick) { // main game tick / frame function called by the browser repeatedly while playing
+            game.counter++;
+            const sinceLastTick = thisTick - game.lastTick; // Store time in ms since last tick
+            game.lastTick = thisTick; // Save new tick
+
+            if(game.counter % 50 == 0) {
+                //Regular Checks, every 50 frames
+                game.ctx.clearRect(0, 0, game.cWidth, game.cHeight);
+                game.ctx.fillStyle = "lightblue";
+                game.ctx.font = "20px Audiowide";
+                game.ctx.textAlign = "right";
+                game.ctx.fillText("FPS: " + Math.trunc(1000 / sinceLastTick), game.cWidth - 10, 30, game.cWidth/2 - 20);
+            }
+
+            requestAnimationFrame(game.tick); // New tick
+        },
+    };
+
+    requestAnimationFrame(game.tick);
+
 }
